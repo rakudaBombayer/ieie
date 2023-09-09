253460c15981865d9da281bba5259f2ce7652645
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-09-09 14:53:49 +0900

    Initial Commit

---
+++ ieie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ieie
+//
+//  Created by  ooie fumiya on 2023/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
