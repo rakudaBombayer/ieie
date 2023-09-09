f13a965b9f929fce65b26277d031728746c3f08b
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-09-09 15:51:30 +0900

    テスト

--- ieie/ViewController.swift
+++ ieie/ViewController.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func upload(_ sender: Any) {
+        let storage = Storage.storage()
+        let reference = storage.reference(forURL: "gs://ieie-69181.appspot.com")
+        
+        let child = reference.child("upload.png")
+        let data = UIImage(named: "uchidenokoduchi_eto01_nedumi")!.pngData()!
+        
+        child.putData(data, metadata: nil){ (metadata,nil) in
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
